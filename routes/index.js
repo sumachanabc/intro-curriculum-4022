87b2068481035cc3f0f424482bbbec7ada48b679
sumachanabc <nishida@uniaim.co.jp>
2024-08-15 23:54:13 +0900

    回答

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ const express = require('express');
 const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient({ log: [ 'query' ] });
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault('Asia/Tokyo');
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
@@ -11,6 +17,9 @@ router.get('/', async (req, res, next) => {
     const schedules = await prisma.schedule.findMany({
       where: { createdBy: parseInt(req.user.id) },
       orderBy: { updatedAt: 'desc' }
+    });
+    schedules.forEach((schedule) => {
+      schedule.formattedUpdatedAt = dayjs(schedule.updatedAt).tz().format('YYYY/MM/DD HH:mm');
     });
     res.render('index', {
       title: title,
